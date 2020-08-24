ce6c888c6663f0f0a10f3ee98f9e973871a2fbd0
Dudi <dudiko2@gmail.com>
2020-08-24 19:54:52 +0300

    canvas now clears

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 import Canvas from "./components/Canvas/Canvas";
 import Toolbar from "./components/Toolbar/Toolbar";
 
 function App() {
+	const [cvsElements, setCvsElements] = useState([]);
+	const [selected, setSelected] = useState(null);
+	const [isMouseDown, setIsMouseDown] = useState(false);
+	const [mouseAnchor, setMouseAnchor] = useState(null);
+
+	const createRect = (e) => {
+		const newElement = {
+			id: Math.random() * 10000,
+			x: e.clientX,
+			y: e.clientY,
+			w: 0,
+			h: 0,
+		};
+
+		setSelected(newElement.id);
+		setCvsElements([...cvsElements, newElement]);
+		setMouseAnchor({ x: newElement.x, y: newElement.y });
+		setIsMouseDown(true);
+	};
+
+	const editRect = (e) => {
+		if (isMouseDown) {
+			const id = cvsElements.findIndex((el) => el.id === selected);
+
+			const x = Math.min(mouseAnchor.x, e.clientX);
+			const y = Math.min(mouseAnchor.y, e.clientY);
+			const w = Math.abs(mouseAnchor.x - e.clientX);
+			const h = Math.abs(mouseAnchor.y - e.clientY);
+
+			const newArr = [...cvsElements];
+
+			newArr[id] = { ...newArr[id], x, y, w, h };
+			setCvsElements(newArr);
+		}
+	};
+
+	const mouseUp = () => {
+		setIsMouseDown(false);
+	};
+
 	return (
 		<div className="App">
 			<div
@@ -21,7 +61,12 @@ function App() {
 			>
 				<Toolbar />
 			</div>
-			<Canvas />
+			<Canvas
+				elements={cvsElements}
+				createObject={createRect}
+				editObject={editRect}
+				onMouseUp={mouseUp}
+			/>
 		</div>
 	);
 }
